3ddd5e6ea7011dadc785626028a0718e82793bcb
KrsPatrick <lesiewicz.patrick@gmail.com>
2022-06-09 13:33:52 +0200

    updated file

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const MONGO_URL = 'mongodb+srv://Patrick:root@cluster0.g4r8jmh.mongodb.net/nasa?retryWrites=true&w=majority'
+const MONGO_URL = 'mongodb+srv://<name>:<passoword>@cluster0.g4r8jmh.mongodb.net/<dbName>?retryWrites=true&w=majority'
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!')
