c6b8253de9797f0e2eb7ffe68d9cbd3c11321d6d
KrsPatrick <lesiewicz.patrick@gmail.com>
2022-06-18 14:13:49 +0200

    added Docker

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
-require('dotenv').config()
+require('dotenv').config() 
 
 const MONGO_URL = process.env.MONGO_URL
+
 
 mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!')
